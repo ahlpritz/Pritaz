ac12d1f16eb0052566f81cfab43615d18e9d2199
ahl pritz solon <ztirp1@gmail.com>
2023-05-22 01:10:16 +0800

    Add project files.

---
+++ Pritaz/Pages/Contact.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Pritaz.Pages
+{
+    public class ContactModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
