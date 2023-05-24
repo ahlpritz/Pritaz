4c9ef2549a52d93b2992209d59bef0912347e9b7
ahl pritz solon <ztirp1@gmail.com>
2023-05-25 00:30:29 +0800

    done with all,

---
+++ Pritaz/Pages/ProjectsPages/GreenlhandDetails.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Pritaz.Pages.ProjectsPages
+{
+    public class GreenlhandDetailsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Pritaz/Pages/ProjectsPages/IoTGarpo.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Pritaz.Pages.ProjectsPages
+{
+    public class IoTGarpoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
